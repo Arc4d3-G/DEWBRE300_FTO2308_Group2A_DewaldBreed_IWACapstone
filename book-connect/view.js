232e8da59263a22781790bc36a35d86a320bf0a1
Dewald Breed <brddew8@protonmail.com>
2023-11-24 10:24:08 +0200

    Pre organisation commit

--- book-connect/view.js
+++ book-connect/view.js
@@ -1,5 +1,5 @@
 import { BOOKS_PER_PAGE, authors, genres, books } from "./data.js";
-// import {matches, page} from "./scripts.js";
+import {matches, page} from "./scripts.js";
 /**
  * An object literal containing references to the relevant HTML elements
  */
@@ -38,7 +38,82 @@ export const html = {
     }
 }
 
+export const css = {
+    day: {
+        dark: '10, 10, 20',
+        light: '255, 255, 255',
+    },
 
+    night: {
+        dark: '255, 255, 255',
+        light: '10, 10, 20',
+    }
+}
+
+// PREVIEW
+/**
+ * Function that creates HTML for each book in data.js that will then
+ * be appended to the list-items div in our page main body.
+ * 
+ * @returns {HTMLElement}
+ */
+export const createPreviewHTML = (targetObject, rangeStart, rangeEnd) => {
+    const listFragment = document.createDocumentFragment()
+    const extractedBooks = targetObject.slice(rangeStart, rangeEnd)
+    for (let { author, image, title, id } of extractedBooks) {
+        author = authors[author] // to display the author as a name and not an id
+
+        const preview = document.createElement('button')
+        preview.className = 'preview'
+        preview.dataset.id = id
+
+        preview.innerHTML = /* html */ `
+        <img
+                class="preview__image"
+                src="${image}"
+            />
+            
+            <div class="preview__info">
+                <h3 class="preview__title">${title}</h3>
+                <div class="preview__author">${author}</div>
+            </div>
+        `
+        listFragment.appendChild(preview)
+
+    }
+    return listFragment
+}
+
+export const generateSearchResults = (filters) => {
+    
+    let result = []
+    filters.title = filters.title.trim()
+    for (const singleBook of books) {
+        const titleMatch = filters.title === '' || singleBook.title.toLowerCase().includes(filters.title.toLowerCase())
+        const authorMatch = filters.author === 'any' || singleBook.author === filters.author
+        let genreMatch = filters.genre === 'any' 
+
+        for (const singleGenre of singleBook.genres) {
+             if (genres[singleGenre] === genres[filters.genre]) { 
+                genreMatch = true 
+            }
+        } 
+        if (titleMatch && authorMatch && genreMatch) {result.push(singleBook)}
+    }
+    
+ return result
+}
+
+export const updateRemaining = () => {
+    const initial = matches.length - (page * BOOKS_PER_PAGE)
+    const remaining = (initial > 0) ? initial : 0
+    html.list.button.disabled = !(remaining > 0)
+    
+    html.list.button.innerHTML = /* html */ `
+        <span>Show more</span>
+        <span class="list__remaining"> (${remaining})</span>
+    `
+}
 
 // GENRE FILTER OPTIONS
 
